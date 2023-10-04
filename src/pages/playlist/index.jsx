6bac7e3ea91ac3771e61d708c90716446c4f89f9
Alisher <93440110+AlisherSrc@users.noreply.github.com>
2023-10-04 23:20:10 +0600

    added functions to the player
    skip forward,backward and pause

--- src/pages/playlist/index.jsx
+++ src/pages/playlist/index.jsx
@@ -2,22 +2,55 @@ import styles from './playlist.module.css';
 
 
 import { useParams } from 'react-router-dom';
-import { storage } from '../../config/firebase';
+import { db, storage } from '../../config/firebase';
+import { useEffect, useRef, useState } from 'react';
+import { collection, getDocs, query, where } from 'firebase/firestore';
 
 const Playlist = () => {
     const { id } = useParams();
-    
+    const [songs, setSongs] = useState([]);
+    const refAudio = useRef(null);
+
+    useEffect(() => {
+        const docs = [];
+
+        const getSongs = async () => {
+            const q = query(collection(db, "song"), where('playlistID', '==', id));
+
+            const querySnapshot = await getDocs(q);
+
+            querySnapshot.forEach((doc) => {
+                docs.push({ id: doc.id, ...doc.data() });
+            });
+
+            setSongs(docs);
+        }
+
+        getSongs();
+    }, []);
+
 
 
 
     return <div>
         Playlist number: {id}
-        <div className={`${styles.audio_player}`}>
-            <audio id="audio" controls>
-                <source src="https://firebasestorage.googleapis.com/v0/b/pulse-palette-f1982.appspot.com/o/God%20knows%20The%20Melancholy%20of%20Haruhi%20Suzumiya%20Kadokawa%20MAD.mp3?alt=media&token=3ffed828-76ef-4dd3-bb89-09b4427af1c6" type="audio/mp3" />
+
+        {songs.map((song) => (
+            <div className={`${styles.audio_player}`} key={song.id}>
+                <audio controls ref={refAudio}>
+                    <source src={song.songFile} type="audio/mp3" />
                     Your browser does not support the audio element.
-            </audio>
-        </div>
+                </audio>
+
+                <button onClick={() => refAudio.current.currentTime += 15}>Skip forward</button>
+                
+                <button onClick={() => refAudio.current.currentTime -= 15}>Skip backward</button>
+                
+                <button onClick={() => refAudio.current.pause()}>Pause</button>
+
+                <p>{refAudio && `${Math.floor(refAudio.current.duration / 60)}:${Math.round(refAudio.current.duration - Math.floor(refAudio.current.duration / 60) * 60)}`}</p>
+            </div>
+        ))}
     </div>
 }
 
