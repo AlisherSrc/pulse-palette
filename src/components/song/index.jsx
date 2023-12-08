50540a095e87ba3cb1053c1f883588ee8fd427ca
Alisher <93440110+AlisherSrc@users.noreply.github.com>
2023-12-08 16:53:29 +0600

    now user can unlike the song

--- src/components/song/index.jsx
+++ src/components/song/index.jsx
@@ -6,43 +6,48 @@ import 'react-h5-audio-player/lib/styles.css';
 import heart from './../../images/heart.svg';
 import filledHeart from './../../images/filled_heart.svg'
 
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { collection, doc, getDocs, query, updateDoc, where } from 'firebase/firestore';
 import { db } from '../../config/firebase';
 import { Context } from '../../App';
 
 const Song = (props) => {
-    const [liked,setLiked] = useState(false);
+    const [liked, setLiked] = useState(false);
 
-    const {customUser} = useContext(Context);
+    const { customUser } = useContext(Context);
 
     const { song } = props;
-
+    // Check if the song is already liked 
+    useEffect(() => {
+        setLiked(customUser.likedSongs.includes(song.id));
+    },[customUser.likedSongs,song.id])
 
     const handleLike = async (songId) => {
         console.log(songId);
-        setLiked(!liked);
-
-  
-        const q = query(collection(db,'user'),where('email','==',customUser.email));
-        const documents = await getDocs(q);
-
-        let user = null;
-
-        documents.forEach((doc) => {
-            user = {
-                id: doc.id,
-                ...doc.data()
+    
+        // Optimistic update
+        setLiked(currentLiked => !currentLiked);
+    
+        try {
+            const userRef = doc(db, 'user', customUser.id);
+            let updatedLikedSongs;
+    
+            if (!liked) {
+                // If currently not liked, add the song
+                updatedLikedSongs = [...customUser.likedSongs, songId];
+            } else {
+                // If currently liked, remove the song
+                updatedLikedSongs = customUser.likedSongs.filter((song) => song !== songId);
             }
-        })
-
-        const userRef = doc(db,'user',user.id);
-
-        await updateDoc(userRef,{
-            likedSongs: [...customUser.likedSongs,songId]
-        })
-
-        console.log("Liked!");
+    
+            await updateDoc(userRef, { likedSongs: updatedLikedSongs });
+    
+            console.log(liked ? "Unliked!" : "Liked!");
+        } catch (error) {
+            console.error("Error updating liked songs:", error);
+            // Optionally revert the optimistic UI update in case of error
+            setLiked(currentLiked => !currentLiked);
+        }
     }
 
     return <>
