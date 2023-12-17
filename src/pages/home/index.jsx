f09f352e613a1c185b3291597b4bc4e4f626f920
Alisher <93440110+AlisherSrc@users.noreply.github.com>
2023-12-17 10:26:51 +0600

    avatar loads after landing on home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,8 +4,29 @@ import Group from '../../components/group';
 import './home.module.css';
 
 import useGetLatestPlaylists from '../../hooks/useGetLatestPlaylists';
+import { useContext, useEffect } from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { collection, getDocs, query, where } from 'firebase/firestore';
+import { Context } from '../../App';
+import { db } from '../../config/firebase';
 
 const Home = () => {
+    const auth = getAuth();
+    const { setCustomUser } = useContext(Context);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, async (user) => {
+            if (user) {
+                const q = query(collection(db, "user"), where("email", '==', user.email));
+                const userDoc = await getDocs(q);
+                userDoc.forEach((user) => {
+                    setCustomUser({ id: user.id, ...user.data() })
+                })
+            }
+        });
+
+        return () => unsubscribe();
+    }, [auth]);
 
    const {playlists,loading} = useGetLatestPlaylists();
 
