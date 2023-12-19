1b31cd6f20476a60d65209ee7ddd534380567ccc
Alisher <93440110+AlisherSrc@users.noreply.github.com>
2023-12-19 09:07:13 +0600

    reset

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import copy from 'vite-plugin-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    copy({
+      targets: [
+        { src: 'pwa.js', dest: 'dist' } // Adjust if your pwa.js is in a subdirectory
+      ],
+      hook: 'writeBundle' // use the 'writeBundle' hook to copy after Vite build
+    })
+  ],
 })
